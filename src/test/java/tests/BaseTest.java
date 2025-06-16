c3add48539bafb6304080bda49ccdaac30574d84
f0ster <f0rsteryo@gmail.com>
2025-06-16 20:24:01 +0300

    changed BaseTest

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -45,11 +45,14 @@ public class BaseTest {
             options.addArguments("--disable-popup-blocking");
             options.addArguments("--disable-infobars");
             options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
             driver = new ChromeDriver(options);
         } else if (browser.equalsIgnoreCase("edge")) {
             EdgeOptions options = new EdgeOptions();
             options.addArguments("--headless");
-            driver = new EdgeDriver();
+            driver = new EdgeDriver(options);
         }
 
         context.setAttribute("driver", driver);
